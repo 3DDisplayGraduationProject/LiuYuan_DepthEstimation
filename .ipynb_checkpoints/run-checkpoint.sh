71d6789e78b0e3b4c00bbbdc39e4d38e2997f29f
liuyuan.merry <liuyuan.merry@bytedance.com>
2020-05-25 17:36:49 +0800

    first commit

---
+++ .ipynb_checkpoints/run-checkpoint.sh
@@ -0,0 +1,2 @@
+
+CUDA_VISIBLE_DEVICES=1 python3 train.py --model_name mono_model --data_path /mnt/cephfs_new_wj/lab_ad_idea/yuanliu/kitti_data --png --log_dir /mnt/cephfs_new_wj/lab_ad_idea/yuanliu/logfile/monodepth2_v0.3  --num_epochs 100 
